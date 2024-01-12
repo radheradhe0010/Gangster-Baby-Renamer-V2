242f7b07665a43d6681ef88ecdc5e262e4887be6
Radhe Radhe <155654271+radheradhe0010@users.noreply.github.com>
2024-01-13 00:21:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "6918817305:AAFecywHiuvIROIcNxQyyZZu86K3wHgAGPc")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "23657837"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "d90bf753f0578c231a458fbb2d023e6f")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "@renamer_all_bot")
 
 
 
